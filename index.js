2ad7a80fdb84f2c8ecd796c18f22f492574b65d6
Keshav46 <GundrukTechchannel@gmail.com>
2022-09-15 15:47:56 +0545

    uploaded

---
+++ index.js
@@ -0,0 +1,166 @@
+//1.ways to print in javascript
+    //console.log("hello world.") 
+    // document.write("its me keshav.")
+    // alert("me")
+//2. javascript console API
+// console.log("hello world",4+6,"another log");ss
+// console.warn("this is warning.");
+// console.error("this is an error.");
+
+//3.Javascript variable
+// var number1= 34;
+// var number2 = 25;
+// console.log(number1+number2);
+
+
+// var num1 = 455;
+// var num2 = 5.5;
+//  var str1 = 'this is a string';
+//  var str2 = 'this is another way to message';
+
+//4.object
+// var marks ={
+//     ravi=78,
+//     keshav=99,
+//     harry=99.9
+// 
+// var und=undefined;
+// console.log(und);
+
+//6.array
+// var arr=[1,2,"keshav",3,4,5]
+
+//7.operator in js
+// var a=32;
+// var b=34;
+// var c=43;
+// console.log("the value of a+b", a+b);
+// console.log("the value of a+b", a-b);
+
+//8.Assignment operator
+// var c=b;
+// c+=3;
+// console.log(c);
+// var a=23;
+// var b=34;
+// console.log(a==b);
+
+
+//9.logical operator
+//logical and
+// console.log(true && true);
+// console.log(false && true);
+
+//logical or
+// console.log(true || true);
+// console.log(false || true);
+
+//logical not
+// console.log(!false);
+// console.log(!false);
+// console.log(!false);
+
+//10. function in the js
+// function avg(a,b){
+//     c=(a+b)/2;
+//     return c;
+// }
+// c1=avg(4,6);
+// c2=avg(14,16);
+// console.log(c1,c2);
+
+//11.condition in js
+// var age=40;
+// if(age<18){
+//     console.log("you are kid");
+// }
+//  else
+//    console.log("you can do it");
+  
+
+//12.loop in js
+// var arr =[1,2,3,5,6,7];
+// console.log("The given array number are:");
+// for(var i=0;i<arr.length;i++)
+// {
+   
+//     console.log(arr[i]);
+// }
+// arr.forEach(function(element){
+//      console.log(element);
+// })
+
+// let j=0;
+// const ac=0;
+// ac=ac+1;
+
+// var i=0;
+// while (i<Array.length)
+//     console.log("keshav");
+//     i++;
+
+// let arr= ["Fan","camet",32,null];
+//array method
+// console.log(arr.length);
+// arr.pop()
+// arr.push("keshav")
+// console.log(arr) 
+// console.log(arr);
+
+//12.string method in javascipt
+// let my = "Harry is a good boy";
+// console.log(mylovelystring.length)
+// console.log(mylovelystring.indexOf("is"))
+// console.log(mylovelystring,slice(1,3))
+// d=my.replace("Harry","keshav")
+// console.log(d,my)
+
+//13.date in javascrpt
+// let mydate= new Date();
+// console.log(mydate);
+// console.log(mydate.getTime());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getDay());
+// console.log(mydate.getMinutes());
+
+
+//14.event in js
+// function clicked(){
+//     console.log("you clicked the button")
+// }
+
+// firstcontainer.addEventListener('click',function(){
+//     console.log("click vayo")
+// })
+
+// firstcontainer.addEventListener('mouseout' ,function(){
+//     console.log("mouse out of container")
+// })
+// firstcontainer.addEventListener('mouseup',function(){
+//     console.log("mouse up")
+
+// })
+// function sum(a,b){
+//     return a+b
+// }
+
+//set timeout and set time interval 
+// loggar=()=>{
+//     console.log("Its me")
+// }
+// clr=setTimeout(loggar,4000);
+
+//local storage in javascript
+// localStorage.setItem('keshav','poudel')
+// undefined
+// localStorage
+// Storage {keshav: "poudel", length: 1}
+//localstirae.clear();
+//json
+// obj={keshav: "poudel", length: 1,a:{this:'th\\t'}}
+// jso=JSON.stringify(obj)
+// console.log(typeof jso)
+// console.log(jso)
+
+a=40
+console.log('this is my $(a)')
---
+++ keshav.py
@@ -0,0 +1,5 @@
+import array as arr
+value=arr.array('i',[])
+n= int(input("enter you value:"))
+for i in range(n):
+  print(value)
